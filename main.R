0877634b464f1cf53d3a172540fa11e4bb920298
hubert-crea <hubert@energyandcleanair.org>
2024-01-04 14:19:36 +0800

    -renaming functions

--- main.R
+++ main.R
@@ -12,15 +12,41 @@
 #   adjust_global(pop, res, poll=c("pm25", "no2"), year=year)
 # })
 
+
+a <- get_exposure_map(year=2020,
+                 res=aqexposuremap::RES_2PT5_MIN,
+                 model="rf",
+                 limit_distance_urban=T,
+                 remove_seasalt_dust_contribution=T,
+                 results_folder = glue('results/{model}/'),
+                 suffix = "test")
+
+
+
+aqexposuremap::build_map(res=aqexposuremap::RES_2PT5_MIN,
+                         suffix="_china",
+                         selected_regions = "CN",
+                         year=2020,
+                         model=MODEL_RF)
+
 # Build more accurate version for certain countries --------------------------------------
 
 lapply(seq(2020, 2023), function(year){
-  aqexposuremap::adjust_global(res=aqexposuremap::RES_2PT5_MIN,
+  aqexposuremap::build_map(res=aqexposuremap::RES_30_SEC,
+                               suffix="_china",
+                               selected_regions = "CN",
+                               year=year,
+                               model=MODEL_GAM)
+})
+
+lapply(seq(2020, 2023), function(year){
+  aqexposuremap::build_map(res=aqexposuremap::RES_2PT5_MIN,
                                suffix="_china",
                                selected_regions = "CN",
                                year=year,
                                model=MODEL_RF)
 })
+
 
 
 aqexposuremap::adjust_country("30_sec", "_india", "IND", "IN", 2018)
